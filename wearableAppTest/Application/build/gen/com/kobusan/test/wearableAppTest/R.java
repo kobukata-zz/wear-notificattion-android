e2d29c6a7f1743df03c3f153181c28400d2986ef
kobukata <kobukata@gmail.com>
2015-05-31 23:59:38 +0900

    wearable app - 5月31日

--- wearableAppTest/Application/build/gen/com/kobusan/test/wearableAppTest/R.java
+++ wearableAppTest/Application/build/gen/com/kobusan/test/wearableAppTest/R.java
@@ -16,10 +16,13 @@ public final class R {
     public static final class layout {
         public static final int activity_phone=0x7f030000;
     }
+    public static final class raw {
+        public static final int wearable_app=0x7f050000;
+    }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int notify_message=0x7f050002;
-        public static final int welcome_message=0x7f050001;
+        public static final int app_name=0x7f060000;
+        public static final int notify_message=0x7f060002;
+        public static final int welcome_message=0x7f060001;
     }
     public static final class xml {
         public static final int wearable_app_desc=0x7f040000;
