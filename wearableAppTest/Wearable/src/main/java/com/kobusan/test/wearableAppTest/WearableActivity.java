5a18dcdd7bc377ee89d6b050836c3d03444d980b
kobukata <kobukata@gmail.com>
2015-05-31 22:17:08 +0900

    wearable test fitst commit

---
+++ wearableAppTest/Wearable/src/main/java/com/kobusan/test/wearableAppTest/WearableActivity.java
@@ -0,0 +1,14 @@
+package com.kobusan.test.wearableAppTest;
+
+import android.app.*;
+import android.os.*;
+
+public class WearableActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wearable);
+    }
+}
