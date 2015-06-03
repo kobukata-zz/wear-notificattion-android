4bf61850316251a98b0d2143b4c8c52a33b5b2bf
kobukata <kobukata@gmail.com>
2015-06-04 00:56:45 +0900

    6月4日(木)

--- wearableAppTest/Wearable/src/main/java/com/kobusan/test/wearableAppTest/WearableActivity.java
+++ wearableAppTest/Wearable/src/main/java/com/kobusan/test/wearableAppTest/WearableActivity.java
@@ -3,11 +3,10 @@ package com.kobusan.test.wearableAppTest;
 import android.app.*;
 import android.os.*;
 
-public class WearableActivity extends Activity 
-{
+public class WearableActivity extends Activity {
+		
     @Override
-    protected void onCreate(Bundle savedInstanceState) 
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wearable);
     }
