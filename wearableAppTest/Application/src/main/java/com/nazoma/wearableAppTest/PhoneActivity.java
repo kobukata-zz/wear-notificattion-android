25233a39b1d1b2ea5a5e7acbc7c67d01461bca2e
kobukata <kobukata@gmail.com>
2015-06-07 16:35:56 +0900

    WatchStubView success

--- wearableAppTest/Application/src/main/java/com/nazoma/wearableAppTest/PhoneActivity.java
+++ wearableAppTest/Application/src/main/java/com/nazoma/wearableAppTest/PhoneActivity.java
@@ -8,14 +8,16 @@ public class PhoneActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone);
+         setContentView(R.layout.activity_phone);
 
-		Notification notif = new Notification.Builder(this)
-			.setContentTitle(getResources().getString(R.string.app_name))
-			.setContentText(getResources().getString(R.string.notify_message))
-			.setSmallIcon(android.R.drawable.stat_notify_more)
-			.build();
-		NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		nm.notify(0, notif);
+				/*
+				Notification notif = new Notification.Builder(this)
+					.setContentTitle(getResources().getString(R.string.app_name))
+					.setContentText(getResources().getString(R.string.notify_message))
+					.setSmallIcon(android.R.drawable.stat_notify_more)
+					.build();
+				NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+				nm.notify(0, notif);
+				*/
     }
 }
--- wearableAppTest/Wearable/src/main/java/com/nazoma/wearableAppTest/WearableActivity.java
+++ wearableAppTest/Wearable/src/main/java/com/nazoma/wearableAppTest/WearableActivity.java
@@ -2,13 +2,26 @@ package com.nazoma.wearableAppTest;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
+import android.content.*;
+import android.support.wearable.view.*;
 
-public class WearableActivity extends Activity 
-{
+public class WearableActivity extends Activity {
+		private TextView mTextView;
+		
     @Override
     protected void onCreate(Bundle savedInstanceState) 
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wearable);
+				setContentView(R.layout.activity_wearable);
+				
+				// make a WatchViewStub Instance
+				final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+				stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener(){
+						@Override
+						public void onLayoutInflated(WatchViewStub stub) {
+								mTextView = (TextView) stub.findViewById(R.id.text);
+						}
+				});
     }
 }
